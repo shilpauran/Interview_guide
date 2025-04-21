d1b7479b85b0e69e4014c931bd099fad9a0a1070
I347159 <shilpa.urandoor@sap.com>
2025-04-21 14:35:11 +0530

    interview

---
+++ src/main/java/urandoor/shilpa/java/LamdaExpressions/classes/InterviewQuestions/GroupByLength.java
@@ -0,0 +1,23 @@
+package main.java.urandoor.shilpa.java.LamdaExpressions.classes.InterviewQuestions;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupByLength {
+
+    public void length()
+    {
+        List<String> list = Arrays.asList("a", "ab", "abc", "abcd", "abcde");
+
+        Map<Integer,List<String>> map = list.stream().collect(Collectors.groupingBy(String::length));
+
+        map.forEach((key,value)->{
+            System.out.println(key);
+            System.out.println(value);
+        });
+    }
+}
